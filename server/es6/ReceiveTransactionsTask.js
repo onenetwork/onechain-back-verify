f70dda15b6c1dde1aaaa056d4c800020b3f23829
pdubey <pdubey@onenetwork.com>
2017-12-08 20:27:46 +0530

    consume transaction messages
    consume transaction messages

--- server/es6/ReceiveTransactionsTask.js
+++ server/es6/ReceiveTransactionsTask.js
@@ -1,75 +1,95 @@
 import config from './config';
 import {transactionConsumer} from './TransactionConsumer';
 import {dbconnectionManager} from './DBConnectionManager';
+import {backChainUtil} from  './BackChainUtil';
 
 class ReceiveTransactionsTask {
     constructor() {}
 
-    /**
-     * - Backchain app goes in a loop doing this:
-  1 call https://platform/oms/rest/backchain/v1/consume?limitInKb=1024
-  2 whatever transaction messages are returned, write to mongo
-  3 if hasMorePages=true in return result, go back to step 1
-  4 if not ... sleep for N minutes (maybe 5 minutes), go back to step 1
+    callGetMessages(authenticationToken, backChainURL, callback) {
+        fetch(backChainUtil.returnValidURL(backChainURL + '/oms/rest/backchain/v1/consume?limitInKb=1024'), {
+            method: 'get',
+            headers: new Headers({
+                'Cache-Control': 'no-cache',
+                'Pragma': 'no-cache',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + new Buffer('ProgressiveRetailerVCAdmin' + ':' + 'password').toString('base64')
+            })
+        }).then(function(response) {
+            return response.json();
+        }).then(function(result) {
+            console.log(result);
+            callback(null, result);
+        }).catch(function (err) {
+            callback(err, null);
+        });
+    }
 
+    consumeTransactionMessages(authenticationToken, backChainURL, callback) {
+        let me = this;
+        this.callGetMessages(authenticationToken, backChainURL, function(error, result) {
+            if(error) {
+                console.log(error);
+                if(typeof callback !== 'undefined') {
+                    callback(error, {syncDone : false});
+                }
+            } else {
+                console.log('hasMorePages: ' + result.hasMorePages);
+                me.insertMessages(result.transactionMessages);
+                let lastSyncTimeInMillis = me.insertOrUpdateSettings(authenticationToken, backChainURL);
 
-  Call get messages should be smart enough to keep going when hasMorePages comes back as true. We will use size limit of 2014
-     */
-    callGetMessages(authenticationToken, backChainURL) {
-        return new Promise((resolve, reject) => {
-            let lastSyncTimeInMillis = new Date().getTime();
-            let settingsCollection = null;
-
-            // find last updated time in DB
-            dbconnectionManager.getConnection().collection("Settings").findOne({type: 'applicationSettings'}, function(err, result) {
-                if (err) throw err;
-                if (result) {
-                    settingsCollection = result;
-                    if(result.chainOfCustidy) {
-                        lastSyncTimeInMillis = result.chainOfCustidy.lastSyncTimeInMillis;
-                    }
+                if(result.hasMorePages) {
+                    me.consumeTransactionMessages(authenticationToken, backChainURL);
+                } else if(!result.hasMorePages) {
+                    setTimeout(function() {
+                        me.consumeTransactionMessages(authenticationToken, backChainURL); /*Don't pass callback, because result already sent to client*/
+                    }, config.readFileTimeInMillis);
                 }
 
-                let msgArr = JSON.parse(transactionConsumer.getMessages(lastSyncTimeInMillis));
-                // put transactionSliceObjects json into msgArr json
-                let transactionSliceObjArr = [];
-                for (let i = 0; i < msgArr.length; i++) {
-                    msgArr[i].transactionSliceObjects = [];
-                    for (let j = 0; j < msgArr[i]['transactionSlices'].length; j++) {
-                        msgArr[i].transactionSliceObjects.push(JSON.parse(msgArr[i]['transactionSlices'][j]));
-                    }
+                if(typeof callback !== 'undefined') {
+                    callback(null, {syncDone : true, authenticationToken: authenticationToken, backChainURL: backChainURL, lastSyncTimeInMillis: lastSyncTimeInMillis});
                 }
+            }
+        });
+    }
 
-                var writeValue = null;
-                if(settingsCollection) {
-                    settingsCollection.chainOfCustidy = {
+    insertOrUpdateSettings(authenticationToken, backChainURL) {
+        let lastSyncTimeInMillis = new Date().getTime();
+        let settingsCollection = null;
+
+        /* finding settingsCollection, because there could be "blockChain" related setup already present */
+        dbconnectionManager.getConnection().collection("Settings").findOne({type: 'applicationSettings'}, function(err, result) {
+            if (err) throw err;
+            if (result) {
+                settingsCollection = result;
+            }
+
+            let writeValue = null;
+            if(settingsCollection) {
+                settingsCollection.chainOfCustidy = {
+                    "authenticationToken" : authenticationToken,
+                    "lastSyncTimeInMillis": lastSyncTimeInMillis,
+                    "backChainURL": backChainURL
+                }
+                writeValue = settingsCollection;
+            } else {
+                writeValue = {
+                    "type" : "applicationSettings",
+                    chainOfCustidy: {
                         "authenticationToken" : authenticationToken,
                         "lastSyncTimeInMillis": lastSyncTimeInMillis,
                         "backChainURL": backChainURL
                     }
-                    writeValue = settingsCollection;
-                } else {
-                    writeValue = { "type" : "applicationSettings",
-                        chainOfCustidy: {
-                            "authenticationToken" : authenticationToken,
-                            "lastSyncTimeInMillis": lastSyncTimeInMillis,
-                            "backChainURL": backChainURL
-                        }
-                    }
-                };
+                }
+            };
 
-                //IMPORTANT Don't have a separate collection. Modify chainOfCustody.lastSyncTimeInMillis value instead. Create a helper method
-                dbconnectionManager.getConnection().collection("Settings").updateOne({type: 'applicationSettings'}, writeValue, { upsert: true }, function(err, res) {
-                    if (err) throw err;
-                    if(msgArr.length > 0) {
-                        resolve({messages : msgArr, lastSyncTimeInMillis: lastSyncTimeInMillis, hasMorePages : Math.random() >= 0.5});
-                    } else {
-                        reject();
-                    }
-                });
-                return msgArr;
+            dbconnectionManager.getConnection().collection("Settings").updateOne({type: 'applicationSettings'}, writeValue, { upsert: true }, function(err, res) {
+                if (err) throw err;
+                else if(res) console.log('Setting time updated successfully!');
             });
         });
+
+        return lastSyncTimeInMillis;
     }
 
     insertMessages(transMessages) {
@@ -83,32 +103,6 @@ class ReceiveTransactionsTask {
                 }
             )
         }
-    }
-
-    consumeTransactionMessages(authenticationToken, backChainURL, callback) {
-        let me = this;
-        receiveTransactionsTask.callGetMessages(authenticationToken, backChainURL).then(function(response) {
-            return response;
-        }).then(function(result) {
-            console.log('hasMorePages: ' + result.hasMorePages);
-            me.insertMessages(result.messages);
-            if(result.hasMorePages) {
-                me.consumeTransactionMessages(authenticationToken, backChainURL);
-            } 
-            else if(!result.hasMorePages) {
-                setTimeout(function() {
-                    me.consumeTransactionMessages(authenticationToken, backChainURL); /*Don't pass callback, because result already sent to client*/
-                }, config.readFileTimeInMillis);
-            }
-            if(typeof callback !== 'undefined') {
-                callback(null, {syncDone : true, authenticationToken: authenticationToken, backChainURL: backChainURL, lastSyncTimeInMillis: result.lastSyncTimeInMillis});
-            }
-        }).catch(function (error) {
-            console.log(error);
-            if(typeof callback !== 'undefined') {
-                callback(error, {syncDone : false});
-            }
-        });
     }
 }
 
