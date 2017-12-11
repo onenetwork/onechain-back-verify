e892ed385d21d1e0cd02811e27263163d8ccc45d
Yang Cao <ycao@onenetwork.com>
2017-12-11 17:42:46 -0600

    change user back

--- server/es6/SyncTransactionTaskHelper.js
+++ server/es6/SyncTransactionTaskHelper.js
@@ -23,7 +23,7 @@ class SyncTransactionTaskHelper {
                     'Cache-Control': 'no-cache',
                     'Pragma': 'no-cache',
                     'Content-Type': 'application/x-www-form-urlencoded',
-                    'Authorization': 'Basic ' + new Buffer('BackchainClient' + ':' + 'password').toString('base64')
+                    'Authorization': 'Basic ' + new Buffer('ProgressiveRetailerVCAdmin' + ':' + 'password').toString('base64')
                 }),
                 body: requestHelper.jsonToUrlParams(params)
             }).then(function(response) {
