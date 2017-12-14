a576aa0584d93178de4221efac51c359aecadbd3
pdubey <pdubey@onenetwork.com>
2017-12-14 15:08:35 +0530

    Event popup with max height and a vertical scroll
    Event popup with max height and a vertical scroll

--- server/es6/components/TrackAndVerifyView.jsx
+++ server/es6/components/TrackAndVerifyView.jsx
@@ -229,7 +229,7 @@ const verifyImgFailed = "/images/verify-failed.png";
                         <td style={fieldProps.columns}>{transaction['date']}</td>
                         <td style={Object.assign({},fieldProps.columns, {cursor:'pointer'})}>
                         <div>
-                            <OverlayTrigger  rootClose trigger="click" placement="right" overlay={<Popover id= {i} title={<span><img style={{width: '18px',height:'18px'}} src="../images/event.svg"/>&nbsp;&nbsp;Events:</span>}>
+                            <OverlayTrigger rootClose trigger="click" placement="right" overlay={<Popover id= {i} arrowOffsetTop = '50' className='eventpopover' title={<span><img style={{width: '18px',height:'18px'}} src="../images/event.svg"/>&nbsp;&nbsp;Events:</span>}>
                             <ul style={{paddingLeft: '0px',listStyleType: 'none'}}>{eventList}</ul>
                             </Popover>}> 
                             <img style={{width: '23px',height:'23px'}} src="../images/event-badge.svg"/>
