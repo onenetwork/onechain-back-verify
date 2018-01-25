4b64bf6afb23f4da5e37aec5afd3b043c7442d3d
Sean Durkin <sndurkin@gmail.com>
2018-01-25 13:14:54 -0500

    Fix regression in previous commit.

--- server/es6/BackChainActions.jsx
+++ server/es6/BackChainActions.jsx
@@ -55,7 +55,7 @@ export default class BackChainActions {
         store.verifications.clear();
 
         if(arguments.length == 1 && Array.isArray(arguments[0])) {
-            BackChainActions.loadTransactionsAux(arguments[0]);
+            BackChainActions.loadTransactionsAux(arguments[0], callback);
             return;
         }
 
