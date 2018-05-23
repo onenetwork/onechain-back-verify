adae047c1265a7babb34b5043f3d9aea717d6869
pdubey <pdubey@onenetwork.com>
2018-05-23 12:03:31 +0530

    promise cleanups
    promise cleanups

--- server/es6/BackChainActions.jsx
+++ server/es6/BackChainActions.jsx
@@ -1100,21 +1100,18 @@ export default class BackChainActions {
      */
     @action
     static readBackChainAddressMapping() {
-        return new Promise(function(resolve, reject) {
-            let uri = '/readBackChainAddressMapping';
-            fetch(uri, {
-                method: 'GET'
-            }).then(function(response) {
-                return response.json();
-            }, function(error) {
-                console.error('error reading BackChainAddressMapping');
-                reject(error);
-            }).then(function(result) {
-                if(result.success) {
-                    store.backChainAddressMapping = result.backChainAddressMapping;
-                    resolve(result.backChainAddressMapping)
-                }
-            })
+        let uri = '/readBackChainAddressMapping';
+        fetch(uri, {
+            method: 'GET'
+        }).then(function(response) {
+            return response.json();
+        }, function(error) {
+            console.error('error reading BackChainAddressMapping');
+            reject(error);
+        }).then(function(result) {
+            if(result.success) {
+                store.backChainAddressMapping = result.backChainAddressMapping;
+            }
         })
     }
 }
