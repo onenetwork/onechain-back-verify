fbb6039008d3641da56a2c1572b87d45cac5f44e
pdubey <pdubey@onenetwork.com>
2018-08-01 19:47:41 +0530

    submitted mistakenly
    submitted mistakenly

--- server/es6/DisputeHelper.js
+++ server/es6/DisputeHelper.js
@@ -376,7 +376,7 @@ class DisputeHelper {
         let entName = backChainAccountOfLoggedUser;
         for (let key in backChainAddressMapping) {
             if (backChainAddressMapping.hasOwnProperty(key)) {
-                if (backChainAccountOfLoggedUser && key.toLowerCase() == backChainAccountOfLoggedUser.toLowerCase()) {
+                if (key.toLowerCase() == backChainAccountOfLoggedUser.toLowerCase()) {
                     entName = backChainAddressMapping[key];
                 }
             }
